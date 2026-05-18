9f7f79b0add9af4926ac482723937ebe054c4601
Your Name <maxhamiltonhon@gmail.com>
2026-05-18 16:31:18 -0500

    TypeError: Score.update_score() takes 0 positional arguments but 1 was given

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from turtle import *
-from time import *
+import time
 from random import *
-
+start = time.time()
 def playing_area():
     pen = Turtle()
     pen.ht()
@@ -15,7 +15,7 @@ def playing_area():
     pen.goto(-150, -200)
     pen.end_fill()
 
-### CLASS and FUNCTION DEFINITIONS ###
+## CLASS and FUNCTION DEFINITIONS ###
 
 class Player(Turtle):
     def __init__(self, x, y, color, player_color, screen, right_key, left_key, fire_key,alive):
@@ -31,12 +31,15 @@ class Player(Turtle):
         self.bullets = []
         self.alive = True
         self.st()
+        self.score = 0
+        self.bullets = []
         screen.onkeypress(self.turn_left, left_key)
         screen.onkeypress(self.turn_right, right_key)
         screen.onkeypress(self.fire, fire_key)
 
     def fire(self):
-        self.bullets.append(Bullet(self))
+        if len(self.bullets) < 5 :
+           self.bullets.append(Bullet(self))
 
     def turn_left(self):
         self.left(10)
@@ -45,17 +48,30 @@ class Player(Turtle):
         self.right(10)
 
 class Block(Turtle):
-    def __init__(self, x, y, color):
+    def __init__(self, x, y, color,blocks):
         super().__init__()
         self.ht()
         self.pu()
         self.color(color)
         self.speed(0)
+        self.hues = ["dead","red","orange", color]
         self.shape("square")
         self.goto(x,y)
         self.st()
+        self.health = 3
+        self.blocks = blocks
 
+    def hit(self,blocks,player,scores):
+        self.health -= 1 
+        if self.health == 0:
+            self.die()
+            player.score += 1
+        else:
+            self.color(self.hues[self.health])
 
+    def die(self):
+        self.ht()
+        self.blocks.remove(self)
 
 class Bullet(Turtle):
     def __init__(self, player):
@@ -69,55 +85,109 @@ class Bullet(Turtle):
         self.shape("triangle")
         self.player = player
         self.st()
+        
+
     def move(self):
-        self.forward(10)
+        self.forward(10) 
         if self.xcor() > 150:
-            self.setheading()
-            self.player.bullets.remove(self)
+            self.setheading(180 - (self.heading()))
         if self.xcor() < -150:
-            self.setheading()
-            self.player.bullets.remove(self)
-        if self.ycor() < -200:
-            self.setheading()
-            self.player.bullets.remove(self)
-        if self.ycor() < 200:
-            self.setheading()
-            self.player.bullets.remove(self)
+            self.setheading(180 - (self.heading()))
+        elif self.heading() == 0:
+            self.heading(180)
+        if self.ycor() > 230:
+            self.die()
+        if self.ycor() < -230:
+            self.die()
 
     def die(self):
         self.ht()
-        self.player.bullets.remove(self)
+        if self in self.player.bullets:
+            self.player.bullets.remove(self)
+
+def update():
+    global start
+    if time.time() - start > 3:
+        start = time.time()
+        for block in blocks:
+            block.goto(block.xcor(), block.ycor() - 20)
+    if p1.alive and p2.alive:
+        for bullet in p1.bullets:
+            bullet.move()
+            for block in blocks:
+                if bullet.distance(block) < 20:
+                    bullet.die()
+                    block.hit(blocks, p1, score1)
+                    score1.update_score()
+        for bullet in p2.bullets:
+            bullet.move()
+            for block in blocks:
+                if bullet.distance(block) < 20:
+                    bullet.die()
+                    block.hit(blocks, p2, score2)
+                    score2.update_score()
 
 
 
 
 
+    screen.ontimer(update, 30)
+
+class Score(Turtle):
+    def __init__(self,x,y):
+        super().__init__()
+        self.ht()
+        self.color("orange")
+        self.pu()
+        self.goto(x,y)
+        self.score = 0 
+        self.write(f"Score: {self.score}")
+
+    def update_score():
+        self.clear()
+        self.write(f"Score: {self.score}")
 
 
+
+
+
+def new_row():
+    for x in range(-130, 140, 20):
+        if len(blocks)%3==0:
+            blocks.append(Block(x,190,"lightblue",blocks))
+        elif len(blocks)%3==1:
+            blocks.append(Block(x,190,"blue",blocks))
+        elif len(blocks)%3==2:
+            blocks.append(Block(x,190,"darkblue",blocks))
+
+             
 
 ### PROGRAM ###
+
 screen = Screen()
 screen.bgcolor("teal")
 screen.setup(600,600)
 screen.listen()
 playing_area()
 
+
 bullets = []
 blocks = []
 p1 = Player(-75, -175, "red","red",screen, "d", "a","w",True)
 p2 = Player(75,-175,"blue","blue",screen, "Right","Left","Up",True)
+score1 = Score (200,-200)
+score2 = Score (-230,-200)
+# start = time.time()
+screen.tracer(0)
 for y in range(190, 100, -20):
     for x in range(-130, 140, 20):
         if len(blocks)%3==0:
-            blocks.append(Block(x,y,"lightblue"))
+            blocks.append(Block(x,y,"lightblue",blocks))
         elif len(blocks)%3==1:
-            blocks.append(Block(x,y,"blue"))
+            blocks.append(Block(x,y,"blue",blocks))
         elif len(blocks)%3==2:
-            blocks.append(Block(x,y,"darkblue"))
-while p1.alive and p2.alive:
-    for bullet in bullets:
-        bullet.move()
-    
-
+            blocks.append(Block(x,y,"darkblue",blocks))
+screen.tracer(1)
+update()
 
 screen.exitonclick()
